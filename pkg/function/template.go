a021f954533090d7c0bf6243ddb75f955124f7e8
Matt Moore <mattmoor@vmware.com>
2020-11-25 13:11:12 -0800

    Add SIGTERM handling (#10)
    * Add SIGTERM handling
    
    * Drop the tail format

--- pkg/function/template.go
+++ pkg/function/template.go
@@ -1,28 +1,55 @@
 package function
 
-import "html/template"
+import "text/template"
 
 const packageMain = `
 package main
 
 import (
-       "fmt"
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
         p "{{.Package}}"
 )
 
 func main() {
-	http.HandleFunc("/", p.{{.Function}})
+	// When we get a SIGTERM, cancel the context.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGTERM)
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		<-c
+		cancel()
+	}()
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+	server := &http.Server{
+		Addr: fmt.Sprint(":", port),
+		Handler: http.HandlerFunc(p.{{.Function}}),
+	}
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	// Start the server in a go routine, so that we can monitor for shutdown and gracefully
+	// drain requests on the main thread.
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	// When the context is cancelled we got a SIGTERM, so drain HTTP requests.
+	// Don't use ctx because it is already closed!
+	// TODO(mattmoor): Add a built-in readiness probe handler that starts failing when
+	// ctx is cancelled for ~30s before calling Shutdown.
+	<-ctx.Done()
+	log.Fatal(server.Shutdown(context.Background()))
 }
 `
 
